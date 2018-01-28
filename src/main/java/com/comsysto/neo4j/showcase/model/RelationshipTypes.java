3d4adab5082d70464c8ddc4a20bb19de09eeddd5
jackandyao <786648643@qq.com>
2018-01-28 17:03:04 +0800

    Initial commit

---
+++ src/main/java/com/comsysto/neo4j/showcase/model/RelationshipTypes.java
@@ -0,0 +1,9 @@
+package com.comsysto.neo4j.showcase.model;
+
+/**
+ * @author: rkowalewski
+ */
+public final class RelationshipTypes {
+    public static final String CLICKED = "CLICKED";
+    public static final String RECOMMEND = "RECOMMEND";
+}
